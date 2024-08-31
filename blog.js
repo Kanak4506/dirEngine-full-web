1baeaa1f634f884caa46b7d693669b1bb9664c3b
Kanak4506 <kanaksehrawat456@gmail.com>
2024-08-31 22:07:42 +0530

    changes

--- blog.js
+++ blog.js
@@ -1,3 +1,36 @@
+const menu_btn = document.querySelector('#menu_btn');
+const menu_drop = document.querySelector('.menu_drop');
+const menu_items = document.querySelectorAll('.menu_drop li');
+
+menu_btn.addEventListener('click', () => {
+    if (menu_drop.classList.contains('hidden')) {
+        menu_drop.classList.remove('hidden');
+
+        gsap.fromTo(menu_drop,
+            { height: '0px' },
+            { height: 'auto', duration: 0.5, ease: 'power1.out' }
+        );
+
+        gsap.fromTo(menu_items,
+            { opacity: 0, y: -20 },
+            { opacity: 1, y: 0, duration: 0.3, stagger: 0.1 }
+        );
+    } else {
+        gsap.to(menu_drop, {
+            height: '0px',
+            duration: 0.5,
+            ease: 'power1.out',
+            onComplete: () => {
+                menu_drop.classList.add('hidden');
+            }
+        });
+        gsap.fromTo(menu_items,
+            { opacity: 1, y: 0 },
+            { opacity: 0, y: -20, duration: 0.1 }
+        );
+    }
+});
+
 
 const scrollTriggerPoint = document.body.scrollHeight * 0.15;
 
